a1ce1abf21424732c8eb5f6d37adf75721f0b5d2
Christian Nirschl <christiannirschl6@gmail.com>
2021-10-21 21:41:13 -0700

    added get request for public files

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT ||3000;
 const path = require('path');
-const htmlRoutes = require("./routes/htmlRoutes");
-const donutRoutes = require("./routes/donutRoutes");
-const logReq = require("./middleware/logRequest")
+const fs = require('fs');
+const util = require('util');
+const db = require('./db/db.json');
+
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+
+app.get('api/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+  app.get('/get', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+
+  app.get('api/notes/', (req,res)=>{
+
+  })
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+});
