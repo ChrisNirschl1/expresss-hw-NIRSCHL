9f615b963b666a1b42c03cc373ab22583e4cc52f
Christian Nirschl <christiannirschl6@gmail.com>
2021-10-21 20:25:40 -0700

    added basic commands to serverjs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT ||3000;
+const path = require('path');
 const htmlRoutes = require("./routes/htmlRoutes");
 const donutRoutes = require("./routes/donutRoutes");
 const logReq = require("./middleware/logRequest")
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
+app.use(express.json());
