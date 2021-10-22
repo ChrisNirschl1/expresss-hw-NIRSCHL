4650ef4ace069b3f6ad4da1ed0f5abdb50ec819a
Christian Nirschl <christiannirschl6@gmail.com>
2021-10-21 22:28:40 -0700

    added helper folder for unique id, trying to link it

--- server.js
+++ server.js
@@ -3,27 +3,31 @@ const app = express();
 const PORT = process.env.PORT ||3000;
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
-const db = require('./db/db.json');
+// const util = require('util');
+// const db = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 
-app.get('api/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
 
-  app.get('/get', (req, res) => {
+  app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
 
   app.get('api/notes/', (req,res)=>{
+    res.json(JSON.parse(fs.readFileSync('./db/notes.json', 'utf8')))
+  })
+
+  app.post('api/notes', (req,res)=>{
 
   })
 
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
